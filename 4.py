d3e60cccf61515513e6cab61f732a0e0b8d5f860
gulshat910 <gulshatdulatova9@gmail.com>
2025-05-15 22:04:32 +0300

    Create 4.py

---
+++ 4.py
@@ -0,0 +1,29 @@
+import re
+
+# Чтение файла text.txt
+with open('text.txt', 'r') as file:
+    text = file.read()
+
+# Поиск email-адресов
+emails = re.findall(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b', text)
+
+# Запись email-адресов в файл emails.txt
+with open('emails.txt', 'w') as file:
+    for email in emails:
+        file.write(email + '\n')
+
+# Поиск номеров телефонов в формате +7-XXX-XXX-XX-XX
+phones = re.findall(r'\+7-\d{3}-\d{3}-\d{2}-\d{2}', text)
+
+# Запись номеров телефонов в файл phones.txt
+with open('phones.txt', 'w') as file:
+    for phone in phones:
+        file.write(phone + '\n')
+
+# Поиск слов с заглавной буквы
+capital_words = re.findall(r'\b[A-Z][a-z]*\b', text)
+
+# Запись слов с заглавной буквы в файл capital_words.txt
+with open('capital_words.txt', 'w') as file:
+    for word in capital_words:
+        file.write(word + '\n')
